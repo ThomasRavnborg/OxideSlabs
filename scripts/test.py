f607854287155e2c42d2086924d3f7a5655510f3
Thomas Ravnborg <s214434@student.dtu.dk>
2026-02-09 17:21:23 +0100

    2026-02-09 17:21:23

--- scripts/test.py
+++ scripts/test.py
@@ -1,5 +1,8 @@
 # Write a .txt file in /results
 import os
 dir = os.getcwd()
+# Go up one level from the current directory
+dir = os.path.dirname(dir)
+
 with open(os.path.join(dir, 'results', 'test.txt'), 'w') as f:
     f.write('This is a test file.')
