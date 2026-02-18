43a1cbe3751c0331dd42e7be4c2904cc352cafe8
Thomas Ravnborg <s214434@student.dtu.dk>
2026-02-18 15:38:45 +0100

    2026-02-18 15:38:45

--- src/structureoptimizer.py
+++ src/structureoptimizer.py
@@ -9,6 +9,11 @@ from ase.units import Ry
 from ase.optimize import BFGS
 from ase.filters import FrechetCellFilter
 from ase.parallel import parprint
+import os
+print('Structureopt enviroment paths:')
+print(os.environ['GPAW_SETUP_PATH'])
+print(os.environ['PATH'])
+print(os.environ['PYTHONPATH'])
 # GPAW
 from gpaw import GPAW
 # Custom modules
