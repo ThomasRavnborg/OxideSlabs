d8299273ba0289921321229853705053d6cc9c52
Thomas Ravnborg <s214434@student.dtu.dk>
2026-02-19 11:38:54 +0100

    2026-02-19 11:38:54

--- scripts/rungpaw.py
+++ scripts/rungpaw.py
@@ -6,29 +6,33 @@ from src.structureoptimizer import perovskite, relax_ase
 from src.bandscalc import calculate_bands
 from src.phononcalc import calculate_phonons
 
-def run(formula):
+def run(formula, task):
     """Function to run the entire workflow for a given perovskite formula.
     Parameters:
     - formula: Chemical formula of the perovskite ('ABX3').
     Returns:
     - None. The function performs the relaxation, band structure calculation, and phonon calculation, and saves the results to files.
     """
-    # Create atoms object for the given formula
-    atoms = perovskite(formula)
-    # Run relaxation using GPAW for atomic positions and cell optimization
-    relax_ase(atoms, xcf='PBEsol',
-             MeshCutoff=100, kgrid=(10, 10, 10),
-             mode='pw', dir=f'results/bulk/GPAW')
-    # Read relaxed structure
-    relaxed_atoms = read(f'results/bulk/GPAW/{formula}.xyz', index=0)
+    if task == 'relax':
+        # Create atoms object for the given formula
+        atoms = perovskite(formula)
+        # Run relaxation using GPAW for atomic positions and cell optimization
+        relax_ase(atoms, xcf='PBEsol',
+                MeshCutoff=100, kgrid=(10, 10, 10),
+                mode='pw', dir=f'results/bulk/GPAW')
+    else:
+        # Read relaxed structure
+        relaxed_atoms = read(f'results/bulk/GPAW/{formula}.xyz', index=0)
+    if task == 'bands':
+        # Calculate band structure and PDOS
+        calculate_bands(relaxed_atoms, xcf='PBEsol',
+                        MeshCutoff=100, kgrid=(10, 10, 10),
+                        mode='pw', dir=f'results/bulk/GPAW')
+    if task == 'phonons':
+        # Calculate phonon dispersion
+        calculate_phonons(relaxed_atoms, xcf='PBEsol', basis='DZP',
+                        MeshCutoff=100, kgrid=(10, 10, 10),
+                        mode='pw', dir=f'results/bulk/GPAW')
 
-    # Calculate band structure and PDOS
-    calculate_bands(relaxed_atoms, xcf='PBEsol',
-                    MeshCutoff=100, kgrid=(10, 10, 10),
-                    mode='pw', dir=f'results/bulk/GPAW')
-    # Calculate phonon dispersion
-    calculate_phonons(relaxed_atoms, xcf='PBEsol', basis='DZP',
-                      MeshCutoff=100, kgrid=(10, 10, 10),
-                      mode='pw', dir=f'results/bulk/GPAW')
-
-run('BaTiO3')
+run('BaTiO3', 'bands')
+run('BaTiO3', 'phonons')
