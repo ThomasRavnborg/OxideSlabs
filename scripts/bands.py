9d6199d2b5703877d3fb5e6770a7a7e9ed236442
Thomas Ravnborg <s214434@student.dtu.dk>
2026-02-25 11:35:30 +0100

    2026-02-25 11:35:30

--- scripts/bands.py
+++ scripts/bands.py
@@ -2,12 +2,9 @@ from ase import Atoms
 from ase.io import read
 from src.bandscalc import calculate_bands
 # Read relaxed structures
-BaTiO3 = read('results/bulk/relaxsiesta/BaTiO3.xyz')
-SrTiO3 = read('results/bulk/relaxsiesta/SrTiO3.xyz')
+BaTiO3 = read('results/bulk/GPAW/BaTiO3.xyz')
 # Calculate band structure and PDOS
 calculate_bands(BaTiO3, xcf='PBEsol', basis='DZP',
                 EnergyShift=0.01, SplitNorm=0.15,
-                MeshCutoff=1000, kgrid=(10, 10, 10))
-calculate_bands(SrTiO3, xcf='PBEsol', basis='DZP',
-                EnergyShift=0.01, SplitNorm=0.15,
-                MeshCutoff=1000, kgrid=(10, 10, 10))
+                MeshCutoff=1000, kgrid=(10, 10, 10),
+                dir='results/bulk/test_bands')
--- scripts/study.py
+++ scripts/study.py
@@ -10,8 +10,6 @@ from src.phononcalc import calculate_phonons
 atoms = perovskite('BaTiO3')
 project = SiestaProject(material=str(atoms.symbols))
 
-project.remove_calculation("0056")
-
 # Define lists of parameters to iterate over
 xcfs =    ['PBEsol']
 basis =   ['TZP']
