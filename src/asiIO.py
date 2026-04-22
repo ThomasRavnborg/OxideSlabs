82756f002a4650b64b72e14808396ba9e4e5f1ad
Thomas Ravnborg <s214434@student.dtu.dk>
2026-04-22 17:29:29 +0200

    Fixed asi implementation, structure filtering and selection

--- src/asiIO.py
+++ src/asiIO.py
@@ -1,28 +1,26 @@
 import numpy as np
 
-def save_asi(A_inv, filename="active_set.asi"):
-    """
-    Save active set inverse matrix (M x M)
-    """
-    A_inv = np.asarray(A_inv)
 
+def save_asi(asi_dict, filename="active_set.asi"):
     with open(filename, "w") as f:
-        n, m = A_inv.shape
-        f.write(f"{n} {m}\n")
+        for atom_type, A_inv in asi_dict.items():
+            n, m = A_inv.shape
+            f.write(f"{atom_type} {n} {m}\n")
+            for val in A_inv.ravel(order="C"):
+                f.write(f"{val}\n")
 
-        for val in A_inv.ravel():
-            f.write(f"{val}\n")
 
-def load_asi(filename):
-    """
-    Load active set inverse matrix (M x M)
-    """
-    with open(filename, "r") as f:
-        n, m = map(int, f.readline().split())
-
-        data = [
-            float(f.readline())
-            for _ in range(n * m)
-        ]
-
-    return np.array(data).reshape(n, m)
+def load_asi(asi):
+    ret = {}
+    with open(asi, "r") as f:
+        while True:
+            B = []
+            line1 = f.readline()
+            if len(line1) == 0:
+                break
+            line1 = line1.split(" ")
+            element, shape1, shape2 = int(line1[0]), int(line1[1]), int(line1[2])
+            for _ in range(shape1 * shape2):
+                B.append(float(f.readline()))
+            ret[element] = np.array(B).reshape((shape1, shape2))
+    return ret
