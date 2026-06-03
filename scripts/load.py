342e2a7915bab09183c2edc9c8cf79853ec7dcb2
Thomas Ravnborg <s214434@student.dtu.dk>
2026-06-03 09:32:17 +0200

    Added test script for load times

---
+++ scripts/load.py
@@ -0,0 +1,22 @@
+import time
+from ase.io import iread
+
+def test_load_time(file_path, type='extended xyz'):
+    t0 = time.time()
+    for i, structure in enumerate(iread(file_path, index=':')):
+        if i == 0:
+            print('ASE atoms object:')
+            print(structure)
+            print('Forces:')
+            print(structure.get_forces())
+            print('Velocities:')
+            print(structure.get_velocities())
+    t1 = time.time()
+    print(f"Time taken to read {type} file: {t1 - t0:.2f} seconds")
+
+test_load_time('results/ALnep/iteration_2/nve_production/Ba8O24Ti8/movie/dump.xyz',
+                type='extended xyz')
+test_load_time('results/ALnep/iteration_2/nve_production/Ba8O24Ti8/exxyz/movie.xyz',
+                type='movie xyz')
+test_load_time('results/ALnep/iteration_2/nve_production_test2/Ba8O24Ti8/500K/movie.nc',
+                type='netcdf')
